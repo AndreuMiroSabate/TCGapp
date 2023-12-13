7b64d4fc4cec6840125ccbe68876690699839e7c
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-13 09:33:59 +0100

    cambios en clase carta
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- tgcapp/lib/api.dart
+++ tgcapp/lib/api.dart
@@ -5,13 +5,13 @@ import 'package:tgcapp/modules/p_card.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<PCard>> apiLoadPCards() async{
-  final List<PCard> pcardList = [];
-  final uri = Uri.parse("https://api.pokemontcg.io/v2/cards?q=set.id:base1");
+  final uri = Uri.parse("https://api.pokemontcg.io/v2/cards?page=1&pageSize=1");
   final response = await http.get(
     uri, 
-    headers: {"X-Api-Key":"8088de87-0aad-4518-be85-6c043f81e54a"});
+    headers: {"X-Api-Key":"8088de87-0aad-4518-be85-6c043f81e54a"},);
   final json = jsonDecode(response.body);
   final jsonPCardList = json["data"];
+  final List<PCard> pcardList = [];
   for (final jsonPCard in jsonPCardList)
   {
     final pcard = PCard.fromJson(jsonPCard);
--- tgcapp/lib/modules/p_card.dart
+++ tgcapp/lib/modules/p_card.dart
@@ -1,15 +1,14 @@
 class PCard {
-  String name, supertype, subtype;
-  dynamic type;
+  String name, supertype;
+  String type;
   String setName;
   String imageUrl;
   String number;
 
   PCard.fromJson(Map<String, dynamic> json)
       : name = json["name"],
-        type = json["types"],
+        type = json["types"][0],
         supertype = json["supertype"],
-        subtype = json["subtypes"],
         setName = json["set"]["name"],
         imageUrl = json["images"]["large"],
         number = json["number"];
