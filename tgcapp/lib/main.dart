43eba9e4cc2065c5d1f1bb5a691f23fb25f53675
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-12-14 21:17:18 +0100

    main update
    Co-Authored-By: AndreuMiroSabate <99949434+AndreuMiroSabate@users.noreply.github.com>

--- tgcapp/lib/main.dart
+++ tgcapp/lib/main.dart
@@ -1,37 +1,24 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:tgcapp/screens/home_Screen.dart';
 import 'package:tgcapp/screens/sets_Screen.dart';
 
 void main(List<String> args) {
-  runApp(TCGApp());
+  runApp(const TCGApp());
 }
 
 // ignore: must_be_immutable
 class TCGApp extends StatelessWidget {
-   TCGApp({super.key});
-
-   String path = "https://api.pokemontcg.io/v2/cards?page=2&pageSize=4";
+  const TCGApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       routes:
-    {
-      "/": (context) => HomeScreen(path: path, tcgapp: this,),
-      "/sets": (context) => StesScreenS(path: path, tcgApp: this),
-    }
+      {
+        "/": (context) => const HomeScreen(),
+        "/sets": (context) => const StesScreenS(),
+      }
     );
   }
-  String getArandomPath()
-{
-  String path;
-  int randpage = Random().nextInt(10)+1;
-  path = "https://api.pokemontcg.io/v2/cards?page=${randpage}&pageSize=4";
-
-  return path;
-}
-
 }
 
