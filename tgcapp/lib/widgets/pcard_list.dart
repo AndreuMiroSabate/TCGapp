48b0acad3711e31419fa6842bf37d31a63677161
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-14 18:07:43 +0100

    changes to card list widget
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- tgcapp/lib/widgets/pcard_list.dart
+++ tgcapp/lib/widgets/pcard_list.dart
@@ -4,13 +4,16 @@ import 'package:tgcapp/api.dart';
 import 'package:tgcapp/modules/p_card.dart';
 import 'package:tgcapp/widgets/card_from_home.dart';
 
+// ignore: must_be_immutable
 class PCardList extends StatelessWidget {
-  const PCardList({super.key});
+  PCardList({super.key, required this.path});
+
+  String path;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-              future: apiLoadPCards(), 
+              future: apiLoadPCards(path), 
               builder: (
                 BuildContext context,
                 AsyncSnapshot<List<PCard>> snapshot,
@@ -23,8 +26,11 @@ class PCardList extends StatelessWidget {
                   return const Center(child: CircularProgressIndicator());
                 }
               final cardList = snapshot.data!;
-              return ListView.builder(
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20),
                 itemCount: cardList.length,
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                
                 itemBuilder: (BuildContext context, int index)
                 {
                   return CardFromHome(pcard: cardList[index]);
