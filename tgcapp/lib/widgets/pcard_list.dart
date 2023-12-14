df61640f84014816066a12314e297f0d12b67385
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-12-14 21:18:21 +0100

    pcard list clean
    Co-Authored-By: AndreuMiroSabate <99949434+AndreuMiroSabate@users.noreply.github.com>

--- tgcapp/lib/widgets/pcard_list.dart
+++ tgcapp/lib/widgets/pcard_list.dart
@@ -13,30 +13,30 @@ class PCardList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-              future: apiLoadPCards(path), 
-              builder: (
-                BuildContext context,
-                AsyncSnapshot<List<PCard>> snapshot,
-              ) {
-                if (snapshot.hasError) {
-                  return ErrorWidget(snapshot.error.toString());
-                }
-                if (!snapshot.hasData)
-                {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              final cardList = snapshot.data!;
-              return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20),
-                itemCount: cardList.length,
-                padding: EdgeInsets.only(top: 20, bottom: 20),
-                
-                itemBuilder: (BuildContext context, int index)
-                {
-                  return CardFromHome(pcard: cardList[index]);
-                },
-              );
-             },
-          );
+        future: apiLoadPCards(path), 
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<List<PCard>> snapshot,
+        ) {
+          if (snapshot.hasError) {
+            return ErrorWidget(snapshot.error.toString());
+          }
+          if (!snapshot.hasData)
+          {
+            return const Center(child: CircularProgressIndicator());
+          }
+        final cardList = snapshot.data!;
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20),
+          itemCount: cardList.length,
+          padding: const EdgeInsets.only(top: 20, bottom: 20),
+          
+          itemBuilder: (BuildContext context, int index)
+          {
+            return CardFromHome(pcard: cardList[index]);
+          },
+        );
+        },
+    );
   }
 }
