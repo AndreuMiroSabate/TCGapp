7e6b3556d21cbba5511b825a207176a6b7b098a0
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-14 18:10:36 +0100

    update home screen
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- tgcapp/lib/screens/home_Screen.dart
+++ tgcapp/lib/screens/home_Screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:tgcapp/widgets/pcard_list.dart';
 
@@ -6,39 +8,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const Align(
-        alignment: Alignment.topCenter,
-        child: Image(image: AssetImage("assets/TCG_Logo.png")),
-      ),
-      Container(
-        height: 30,
-        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
-        margin: const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 30),
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(20),
+    return Scaffold(
+      body: Column(children: [
+        const Align(
+          alignment: Alignment.topCenter,
+          child: Image(image: AssetImage("assets/TCG_Logo.png")),
         ),
-        child: Row(children: [
-          Icon(
-            Icons.search,
-            color: Colors.grey[700],
+        Container(
+          height: 30,
+          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
+          margin: const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 30),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(20),
           ),
-          const Expanded(child: TextField())
-        ]),
-      ),
-      const Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ElevatedButton(onPressed: null, child: Text("Filter")),
-          ElevatedButton(onPressed: null, child: Text("Sets")),
-          ElevatedButton(onPressed: null, child: Text("Favs")),
-        ],
-      ),
-      Expanded(
-        child: PCardList(),
-      ),
-    ]);
+          child: Row(children: [
+            Icon(
+              Icons.search,
+              color: Colors.grey[700],
+            ),
+            const Expanded(child: TextField())
+          ]),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(onPressed: null, child: Text("Filter")),
+            ElevatedButton(onPressed: (){Navigator.of(context).pushNamed("/sets");}, child: Text("Sets")),
+            ElevatedButton(onPressed: null, child: Text("Favs")),
+          ],
+        ),
+        Expanded(
+          child: PCardList(path: "https://api.pokemontcg.io/v2/cards?page=1&pageSize=4"),
+        ),
+      ]),
+    );
   }
+}
+
+String getArandomPath()
+{
+  String path;
+  int randpage = Random().nextInt(5);
+  path = "https://api.pokemontcg.io/v2/cards?page=${randpage}Size=4";
+
+  return path;
 }
