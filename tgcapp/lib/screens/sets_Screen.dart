335219a1e3ab4f2ebba4833e041c7abbc0bca071
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-12-14 21:17:46 +0100

    set screen clean up
    Co-Authored-By: AndreuMiroSabate <99949434+AndreuMiroSabate@users.noreply.github.com>

--- tgcapp/lib/screens/sets_Screen.dart
+++ tgcapp/lib/screens/sets_Screen.dart
@@ -1,42 +1,12 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:tgcapp/main.dart';
+import 'package:tgcapp/widgets/functions_class.dart';
 import 'package:tgcapp/widgets/pset_list.dart';
 
 // ignore: must_be_immutable
-class SetsScreen extends StatelessWidget {
-  SetsScreen({super.key, required this.path, required this.tcgApp});
-
-  String path;
-  TCGApp tcgApp;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-          children: [
-            ElevatedButton(
-              onPressed:(){
-                Navigator.pop(context);
-                //tcgApp.changepath(tcgApp.getArandomPath());
-              },
-              child:const Icon(Icons.arrow_back, size: 20),
-              ),
-            Expanded(child: PSetList(path: "https://api.pokemontcg.io/v2/sets?orderBy=set.releaseDate"))
-          ],
-      
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
 class StesScreenS extends StatefulWidget {
-  StesScreenS({super.key,required this.path, required this.tcgApp});
-
-  String path;
-  TCGApp tcgApp;
+ const StesScreenS({super.key});
 
   @override
   State<StesScreenS> createState() => _StesScreenSState();
@@ -52,8 +22,8 @@ class _StesScreenSState extends State<StesScreenS> {
             ElevatedButton(
               onPressed:(){
                 setState(() {
-                  widget.path = widget.tcgApp.getArandomPath();
-                  Navigator.pop(context);
+                  cPath = ClassPath(getArandomPath());
+                  Navigator.of(context).pushNamed("/");
                 });  
                 },
               child:const Icon(Icons.arrow_back, size: 20),
