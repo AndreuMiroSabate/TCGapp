0730a290e9e753d6c2b7d784c9c65b1b968e0c33
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-14 20:16:17 +0100

    stateful widget in sets_screen
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- tgcapp/lib/screens/sets_Screen.dart
+++ tgcapp/lib/screens/sets_Screen.dart
@@ -1,10 +1,15 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:tgcapp/main.dart';
 import 'package:tgcapp/widgets/pset_list.dart';
 
+// ignore: must_be_immutable
 class SetsScreen extends StatelessWidget {
-  const SetsScreen({super.key});
+  SetsScreen({super.key, required this.path, required this.tcgApp});
+
+  String path;
+  TCGApp tcgApp;
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +17,49 @@ class SetsScreen extends StatelessWidget {
       body: Column(
           children: [
             ElevatedButton(
-              onPressed:(){Navigator.pop(context);},
+              onPressed:(){
+                Navigator.pop(context);
+                //tcgApp.changepath(tcgApp.getArandomPath());
+              },
               child:const Icon(Icons.arrow_back, size: 20),
               ),
             Expanded(child: PSetList(path: "https://api.pokemontcg.io/v2/sets?orderBy=set.releaseDate"))
           ],
       
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class StesScreenS extends StatefulWidget {
+  StesScreenS({super.key,required this.path, required this.tcgApp});
+
+  String path;
+  TCGApp tcgApp;
+
+  @override
+  State<StesScreenS> createState() => _StesScreenSState();
+}
+
+class _StesScreenSState extends State<StesScreenS> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          children: [
+            ElevatedButton(
+              onPressed:(){
+                setState(() {
+                  widget.path = widget.tcgApp.getArandomPath();
+                  Navigator.pop(context);
+                });  
+                },
+              child:const Icon(Icons.arrow_back, size: 20),
+              ),
+            Expanded(child: PSetList(path: "https://api.pokemontcg.io/v2/sets?orderBy=set.releaseDate"))
+          ],
       ),
     );
   }
