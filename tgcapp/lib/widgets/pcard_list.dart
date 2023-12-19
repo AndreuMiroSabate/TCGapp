085dc2334682186737a0dd1cd863343f9e3e9a33
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-19 18:21:22 +0100

    in case of error noy found
    Co-Authored-By: flishflash <99949318+flishflash@users.noreply.github.com>

--- tgcapp/lib/widgets/pcard_list.dart
+++ tgcapp/lib/widgets/pcard_list.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:tgcapp/api.dart';
 import 'package:tgcapp/modules/p_card.dart';
 import 'package:tgcapp/widgets/card_from_home.dart';
+import 'dart:math';
 
 // ignore: must_be_immutable
 class PCardList extends StatelessWidget {
   PCardList({super.key, required this.path});
 
   String path;
+  int i = Random().nextInt(6)+1;
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +21,17 @@ class PCardList extends StatelessWidget {
           AsyncSnapshot<List<PCard>> snapshot,
         ) {
           if (snapshot.hasError) {
-            return ErrorWidget(snapshot.error.toString());
+            return Image(image: AssetImage("assets/not_found_0$i.png"),);
           }
           if (!snapshot.hasData)
           {
             return const Center(child: CircularProgressIndicator());
           }
         final cardList = snapshot.data!;
+        if(cardList.isEmpty)
+        {
+          return Image(image: AssetImage("assets/not_found_0$i.png"),);
+        }
         return GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20),
           itemCount: cardList.length,
