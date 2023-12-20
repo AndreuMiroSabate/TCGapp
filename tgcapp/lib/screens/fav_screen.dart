dc2206593d8e83e1be392870b50fff7fc8aaf2eb
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2023-12-20 18:00:27 +0100

    Return to home from favs

--- tgcapp/lib/screens/fav_screen.dart
+++ tgcapp/lib/screens/fav_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tgcapp/widgets/functions_class.dart';
 import 'package:tgcapp/widgets/pcard_fav_list.dart';
 
 class FavouriteScreen extends StatefulWidget {
@@ -27,7 +28,8 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
-                      Navigator.pop(context);
+                      cPath = ClassPath(getArandomPath());
+                      Navigator.of(context).pushNamed("/");
                     });
                   },
                   child: const Icon(Icons.arrow_back, size: 20),
